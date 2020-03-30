207e6270a63914e450887d2dfd6c388ae2555b6b
Rafael Puchades Marqués <rpuchadm@cfp.upv.es>
2020-03-30 16:08:34 +0200

    process

--- src/components/TrainContainer.js
+++ src/components/TrainContainer.js
@@ -5,20 +5,32 @@ const STEP_SEPARATOR = '+'
 const LOOP_SEPARATOR = '*'
 
 
-const text = `20' calentamiento + 5*1' msiprogresivo/1'suave + 5' R2 + 5' R1 + 5*1'R3/2'R1 +5'R1`
+const TEXT = `20' calentamiento + 5*1' msiprogresivo/1'suave + 5' R2 + 5' R1 + 5*1'R3/2'R1 +5'R1`
+const processStep = (text) => {
+    let obj = { text: text}
+    // if
+    return obj
+}
+const processSteps = (text) => {
+    const textarray = text.split(STEP_SEPARATOR)
+    const steparray = textarray.map( (text) => processStep( text ) )
+    return steparray
+}
 
-const Step = ({step,i}) => {
-    return(
-        <li>[{i}] {step}</li>
+const Step = ({text,i}) => {
+    return (
+        <li>[{i}] {text}</li>
     )
 }
 Step.propTypes = {
-    steps: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    i: PropTypes.number.isRequired,
 }
 
 const Steps = ({steps}) => {
-    return(
-        <ul>{ steps.map( (step,i) => <Step key={i} step={step} i={i} /> ) }</ul>
+    //return ( <>{JSON.stringify(steps)} </>)
+    return (
+        <ul>{ steps.map( (step,i) => <Step key={i} {...step} i={i} /> ) }</ul>
     )
 }
 Steps.propTypes = {
@@ -26,7 +38,7 @@ Steps.propTypes = {
 }
 
 const TrainContainer = () => {
-    const steps = text.split(STEP_SEPARATOR);
+    const steps = processSteps( TEXT) // text.split(STEP_SEPARATOR);
     return(
         <>
         <h1>TrainContainer</h1>
