ae98b3d17e040246589daffdb0e90e5e4984cde1
Rafael Puchades Marqués <rpuchadm@cfp.upv.es>
2020-03-30 15:53:39 +0200

    steps

--- src/components/TrainContainer.js
+++ src/components/TrainContainer.js
@@ -1,9 +1,38 @@
 import React from "react"
-//import PropTypes from "prop-types"
+import PropTypes from "prop-types"
+
+const STEP_SEPARATOR = '+'
+const LOOP_SEPARATOR = '*'
+
+
+const text = `20' calentamiento + 5*1' msiprogresivo/1'suave + 5' R2 + 5' R1 + 5*1'R3/2'R1 +5'R1`
+
+const Step = ({step,i}) => {
+    return(
+        <li>[{i}] {step}</li>
+    )
+}
+Step.propTypes = {
+    steps: PropTypes.string.isRequired,
+}
+
+const Steps = ({steps}) => {
+    return(
+        <ul>{ steps.map( (step,i) => <Step key={i} step={step} i={i} /> ) }</ul>
+    )
+}
+Steps.propTypes = {
+    steps: PropTypes.array.isRequired,
+}
 
 const TrainContainer = () => {
-    	return(
-            <h1>TrainContainer</h1>
-        )
+    const steps = text.split(STEP_SEPARATOR);
+    return(
+        <>
+        <h1>TrainContainer</h1>
+        <Steps steps={steps} />
+        <br/><br/>
+        </>
+    )
 }
 export default TrainContainer
